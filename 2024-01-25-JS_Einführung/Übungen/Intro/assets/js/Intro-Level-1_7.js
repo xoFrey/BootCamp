33653c6c2494fa8389f1c9ad25f4da576377ac3c
xoFrey <xizelacar@gmail.com>
2024-01-25 12:56:48 +0100

    intro 1-7 done

---
+++ 2024-01-25-JS_Einführung/Übungen/Intro/assets/js/Intro-Level-1_7.js
@@ -0,0 +1,7 @@
+const pi = 3.14;
+const burjDubai = "828m";
+const fullName = "Jan Schmidt";
+const eifelTower = "324m";
+let helloWorld = "Hello World";
+let camelCase = "camelCase";
+let kursStatus = true;
