6156306d5a32d98706fe7dc206e9fe3d6d74325d
xoFrey <xizelacar@gmail.com>
2024-01-25 13:21:45 +0100

    level 1-12 done

---
+++ 2024-01-25-JS_Einführung/Übungen/Arithmetische_Operatoren/assets/js/Intro-Level-1_12.js
@@ -0,0 +1,20 @@
+let score = 5 + 5 * 10;
+console.log("Ergebnis: " + score);
+
+let score2 = (5 + 5) * 10;
+console.log("Ergebnis: " + score2);
+
+let score3 = 0;
+score3 = score3 + 10;
+console.log("Ergebnis: " + score3);
+score3 += 10;
+console.log("Ergebnis: " + score3);
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+zahl++;
+console.log("Increment: " + zahl);
+
+zahl--;
+console.log("Decrement: " + zahl);
