e8fb4ef1f6a1b1f219c10fdabffbf8d0e9712889
xoFrey <xizelacar@gmail.com>
2024-01-25 13:00:16 +0100

    var 1-6 done

---
+++ 2024-01-25-JS_Einführung/Übungen/Intro/assets/js/Variablen-Level-1_6.js
@@ -0,0 +1,9 @@
+// ? Nummer 1
+// let x = 34;
+// x = 67;
+// console.log(x);
+
+// ? Nummer 2
+const y = 34;
+y = 67;
+// - Fehlermeldung, da man const nicht überschreiben kann. Der Wert ist konstant und kann somit nicht verändert werden, sobald er deklariert wurde
