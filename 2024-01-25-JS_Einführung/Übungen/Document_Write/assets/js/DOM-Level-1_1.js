19595fbc71bcbee1ec91e816a6d6a0475d0f209e
xoFrey <xizelacar@gmail.com>
2024-01-25 13:44:08 +0100

    dom1-1 done

---
+++ 2024-01-25-JS_Einführung/Übungen/Document_Write/assets/js/DOM-Level-1_1.js
@@ -0,0 +1,10 @@
+let myText = "Hello again!";
+document.write("<h1> Hello World </h1>");
+document.write("<p> Have a nice day! </p>");
+document.write(myText);
+document.write("<br>");
+
+let firstName = "Izel";
+let lastName = "Acar";
+
+document.writeln("Hi ich bin SuperCoder:in " + firstName + " " + lastName);
