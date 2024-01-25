d913d71de2a25fe243014ba17bf2f5061a3c054e
xoFrey <xizelacar@gmail.com>
2024-01-25 13:14:41 +0100

    intro 1-8 done

---
+++ 2024-01-25-JS_Einführung/Übungen/Arithmetische_Operatoren/assets/js/Intro-Level-1_8.js
@@ -0,0 +1,34 @@
+let x = 20;
+let y = 30;
+// *Addition
+console.log(x + y);
+// *Subtraktion
+console.log(y - x);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(y / x);
+
+let z = 10;
+let resultOne = (x * y) / z;
+console.log(resultOne);
+
+let a = 15;
+let b = 9;
+
+console.log(a % b);
+
+let c = 20;
+let resultTwo = (a + b) * c;
+console.log(resultTwo);
+
+a++;
+console.log(a);
+
+b--;
+console.log(b);
+
+let resultThree = a - b;
+console.log(resultThree);
+
+console.log(resultOne % resultTwo);
