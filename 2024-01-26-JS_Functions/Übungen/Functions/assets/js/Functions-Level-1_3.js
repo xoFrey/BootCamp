f748f58220ecc078fde13047c17fefcccc677484
xoFrey <xizelacar@gmail.com>
2024-01-26 14:10:58 +0100

    functions 1-3 done

---
+++ 2024-01-26-JS_Functions/Übungen/Functions/assets/js/Functions-Level-1_3.js
@@ -0,0 +1,13 @@
+function intro3(name, city, age) {
+  console.log(
+    "Hallo, mein name ist " +
+      name +
+      ". Ich bin " +
+      age +
+      " Jahre alt. Ich komme aus " +
+      city +
+      "."
+  );
+}
+
+intro3("Izel", "Neumünster", 30);
