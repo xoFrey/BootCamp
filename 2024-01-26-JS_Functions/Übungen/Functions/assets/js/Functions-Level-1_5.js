76bdb2c190f74d710f5b173cdac1f827dd42f9c0
xoFrey <xizelacar@gmail.com>
2024-01-26 14:15:32 +0100

    function 1-5 done

---
+++ 2024-01-26-JS_Functions/Übungen/Functions/assets/js/Functions-Level-1_5.js
@@ -0,0 +1,13 @@
+function math(num1, num2) {
+  console.log(
+    "Multiplikation von " + num1 + " und " + num2 + ": " + num1 * num2
+  );
+  console.log("Division von " + num1 + " und " + num2 + ": " + num1 / num2);
+}
+
+math(10, 2);
+math(30, 20);
+math(100, 100);
+math(5, 0);
+math(45, 173);
+math(1, 1000);
