3c1bb1d94f3e2f964a3ec7f60d9034af1214c6e5
xoFrey <xizelacar@gmail.com>
2024-01-26 14:06:12 +0100

    function 1-2 done

---
+++ 2024-01-26-JS_Functions/Übungen/Functions/assets/js/Functions-Level-1_2.js
@@ -0,0 +1,7 @@
+function intro2(paramName) {
+  const name = "SuperCoder:in.";
+  console.log("Hi " + name + " Mein Name ist " + paramName);
+}
+
+intro2("Max Mustermann");
+intro2("Mareike Musterfrau");
