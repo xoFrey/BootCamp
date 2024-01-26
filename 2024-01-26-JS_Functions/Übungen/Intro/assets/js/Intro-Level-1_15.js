b755c42e098f08516c6091c64c8b25d89bd11e49
xoFrey <xizelacar@gmail.com>
2024-01-26 13:59:48 +0100

    intro 1-15 done

---
+++ 2024-01-26-JS_Functions/Übungen/Intro/assets/js/Intro-Level-1_15.js
@@ -0,0 +1,13 @@
+const num = 5;
+const anotherNum = 8;
+
+console.log(num == anotherNum);
+console.log(num === anotherNum);
+console.log(num > anotherNum);
+console.log(num < anotherNum);
+console.log(num >= anotherNum);
+console.log(num <= anotherNum);
+
+console.log(num == 5);
+console.log(num != 8);
+console.log(num != 5);
