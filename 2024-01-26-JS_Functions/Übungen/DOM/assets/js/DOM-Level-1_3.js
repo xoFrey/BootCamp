4ad0a25dfa3aacc35f3db962c02ae4d2591fbf32
xoFrey <xizelacar@gmail.com>
2024-01-26 13:50:30 +0100

    dom 1-3 done

--- 2024-01-26-JS_Functions/Übungen/DOM/assets/js/DOM-Level-1_3.js
+++ 2024-01-26-JS_Functions/Übungen/DOM/assets/js/DOM-Level-1_3.js
@@ -1,3 +1,9 @@
-const figure = document.querySelector("#gallery");
-figure.innerHTML =
+const figure1 = document.querySelector("#gallery");
+figure1.innerHTML =
   "<figure><img src='../../../../../0assets/img/Cute-Cat-icon.png' alt='cat' />  <figcaption>Fig.1</figcaption></figure>";
+const figure2 = document.querySelector("#gallery");
+figure2.innerHTML +=
+  "<figure><img src='../../../../../0assets/img/Cute-Cat-icon.png' alt='cat' />  <figcaption>Fig.2</figcaption></figure>";
+const figure3 = document.querySelector("#gallery");
+figure3.innerHTML +=
+  "<figure><img src='../../../../../0assets/img/Cute-Cat-icon.png' alt='cat' />  <figcaption>Fig.3</figcaption></figure>";
