fd6cfbede289bbad164d53ab32237fb7978e8650
xoFrey <xizelacar@gmail.com>
2024-01-29 13:42:07 +0100

    classlist 2-1 done

---
+++ 2024-01-29-Klassen/Übungen/classList/assets/js/Classlist-Level-2_1.js
@@ -0,0 +1,9 @@
+function changeHeadline() {
+  const headline = document.querySelector("#headline");
+  headline.classList.add("head");
+}
+
+function removeHeadline() {
+  const headline = document.querySelector("#headline");
+  headline.classList.remove("head");
+}
