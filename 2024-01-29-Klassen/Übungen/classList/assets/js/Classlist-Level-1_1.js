a643bf160b2d910e4cab05c570d0b90bb0369986
xoFrey <xizelacar@gmail.com>
2024-01-29 13:32:25 +0100

    classlist1-1 done

---
+++ 2024-01-29-Klassen/Übungen/classList/assets/js/Classlist-Level-1_1.js
@@ -0,0 +1,8 @@
+function addClassHeadline() {
+  const headlineAdd = document.querySelector("#headline");
+  headlineAdd.classList.add("head");
+}
+function addClassList() {
+  const listAdd = document.querySelector("#liste");
+  listAdd.classList.add("list");
+}
