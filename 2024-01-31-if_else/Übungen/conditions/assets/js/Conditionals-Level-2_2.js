a815bde6d40993b276aa8195f39c8392396fd1c4
xoFrey <xizelacar@gmail.com>
2024-02-01 09:37:02 +0100

    u0pdate

--- 2024-01-31-if_else/Übungen/conditions/assets/js/Conditionals-Level-2_2.js
+++ 2024-01-31-if_else/Übungen/conditions/assets/js/Conditionals-Level-2_2.js
@@ -11,17 +11,19 @@ function airQuality() {
     levelHealth.innerHTML = "<h2> Level of health concern: Good</h2>";
     levelHealth.innerHTML +=
       "<h2> Level of health concern: Little or no Risk</h2>";
-  } else if (airValue >= 50 && airValue <= 100) {
+  } else if (airValue > 50 && airValue <= 100) {
     levelHealth.innerHTML = "<h2> Level of health concern: Moderate</h2>";
     levelHealth.innerHTML +=
       "<h2> Level of health concern: Acceptable quality</h2>";
 
     bgColor.style.backgroundColor = "orange";
-  } else {
+  } else if (airValue > 100 && airValue <= 150) {
     levelHealth.innerHTML = "<h2> Level of health concern: Unhealthy</h2>";
     levelHealth.innerHTML +=
       "<h2> Level of health concern: Generable publics not likely affected</h2>";
 
     bgColor.style.backgroundColor = "red";
+  } else {
+    levelHealth.innerHTML = "<h2> Error: Can't measure air quality! </h2>";
   }
 }
