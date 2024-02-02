ff225ba8c7309489a6c6ce3aa5e2b82c3c37488a
xoFrey <xizelacar@gmail.com>
2024-02-02 13:11:11 +0100

    strmeth 1-3 done

---
+++ 2024-02-02-Stringmethods/Übungen/stringmethods/assets/js/String-Methods-Level-1_3.js
@@ -0,0 +1,6 @@
+let txt2 =
+  "Blue, green, grey, white, or black; smooth, ruffled, or mountainous; that ocean is not silent.";
+
+console.log(txt2.search(";"));
+console.log(txt2.search("green"));
+console.log(txt2.search("blue"));
