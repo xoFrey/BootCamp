83221524b793af70cb01cec4e0a36b4deedd80e4
xoFrey <xizelacar@gmail.com>
2024-02-05 11:49:51 +0100

    stringmethods 1-1 done

---
+++ 2024-02-05-return/Übungen/string_methods/assets/js/String-Methods-Level-1_1.js
@@ -0,0 +1,9 @@
+const firstName = "Izel";
+const lastName = "Acar";
+
+console.log(firstName.length);
+console.log(lastName.length);
+
+let fullName = firstName.concat(" ", lastName);
+console.log(fullName);
+console.log(fullName.length);
