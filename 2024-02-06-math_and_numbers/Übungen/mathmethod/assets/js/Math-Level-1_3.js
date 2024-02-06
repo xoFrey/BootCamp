da66c0015791fc050bea1dc6ef0b7f2aaaad86ec
xoFrey <xizelacar@gmail.com>
2024-02-06 12:02:17 +0100

    math1.3 done

---
+++ 2024-02-06-math_and_numbers/Übungen/mathmethod/assets/js/Math-Level-1_3.js
@@ -0,0 +1,8 @@
+const randomNum = Math.floor(Math.random());
+console.log(randomNum);
+
+const randomNum1_10 = Math.ceil(Math.random() * 10);
+console.log(randomNum1_10);
+
+const randomNum1_100 = Math.ceil(Math.random() * 10 * 10);
+console.log(randomNum1_100);
