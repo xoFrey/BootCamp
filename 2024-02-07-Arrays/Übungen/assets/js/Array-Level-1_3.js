1d3b9b4e7cceb097bb2eca844e893487f2430808
xoFrey <xizelacar@gmail.com>
2024-02-07 12:25:11 +0100

    array 1-3 done

---
+++ 2024-02-07-Arrays/Übungen/assets/js/Array-Level-1_3.js
@@ -0,0 +1,7 @@
+const person = ["Tony", "Stark", "Superreich"];
+const friends = ["Farid", "Christian", "Sergio"];
+const favoriteFood = ["Burger", "Sushi", "Pizza"];
+
+console.log(person.length);
+console.log(friends.length);
+console.log(favoriteFood.length);
