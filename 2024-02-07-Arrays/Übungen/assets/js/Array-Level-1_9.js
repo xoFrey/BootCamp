73c1985fea181d72f6135874ad81ae55141a1864
xoFrey <xizelacar@gmail.com>
2024-02-07 12:36:55 +0100

    1.9 done

---
+++ 2024-02-07-Arrays/Übungen/assets/js/Array-Level-1_9.js
@@ -0,0 +1,16 @@
+const myArray = [23, 54, 75];
+console.log(myArray);
+
+myArray.push(11, 32, 42, 5, 71);
+console.log(myArray);
+
+myArray.unshift(1, 3, 7, 8, 14);
+console.log(myArray);
+
+myArray.pop();
+myArray.pop();
+console.log(myArray);
+
+myArray.shift();
+myArray.shift();
+console.log(myArray);
