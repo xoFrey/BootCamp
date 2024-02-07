4207fb1154b15ddec7cfb2948006187f0706a09f
xoFrey <xizelacar@gmail.com>
2024-02-07 12:28:03 +0100

    1-4 done

---
+++ 2024-02-07-Arrays/Übungen/assets/js/Array-Level-1_4.js
@@ -0,0 +1,7 @@
+const person = ["Tony", "Stark", "Superreich"];
+const friends = ["Farid", "Christian", "Sergio"];
+const favoriteFood = ["Burger", "Sushi", "Pizza"];
+
+let newPerson = person.push("Supercool", "Superschlau", "Iron Man");
+console.log(newPerson);
+console.table(person);
