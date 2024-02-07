389927beea5bcf1f279ab3524ad8d8b76f04c0ef
xoFrey <xizelacar@gmail.com>
2024-02-07 12:30:19 +0100

    1-5 done

---
+++ 2024-02-07-Arrays/Übungen/assets/js/Array-Level-1_5.js
@@ -0,0 +1,9 @@
+const person = ["Tony", "Stark", "Superreich"];
+
+let newPerson = person.push("Supercool", "Superschlau", "Iron Man");
+console.log(newPerson);
+console.table(person);
+
+let popPerson = person.pop();
+console.log(popPerson);
+console.table(person);
