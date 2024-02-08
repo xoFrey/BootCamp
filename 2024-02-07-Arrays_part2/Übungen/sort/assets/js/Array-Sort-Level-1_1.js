f2fceba86c67e73b281b195147c886a97e874693
xoFrey <xizelacar@gmail.com>
2024-02-08 13:55:46 +0100

    array 1.2 1.1 done

---
+++ 2024-02-07-Arrays_part2/Übungen/sort/assets/js/Array-Sort-Level-1_1.js
@@ -0,0 +1,19 @@
+let languages = [
+  "JavaScript",
+  "Python",
+  "Java",
+  "Ruby",
+  "PHP",
+  "C++",
+  "CSS",
+  "C#",
+  "Go",
+  "C",
+  "TypeScript",
+  "Swift",
+];
+
+console.table(languages);
+
+languages.sort();
+console.table(languages);
---
+++ 2024-02-07-Arrays_part2/Übungen/sort/assets/js/Array-Sort-Level-1_2.js
@@ -0,0 +1,20 @@
+let languages = [
+  "JavaScript",
+  "Python",
+  "Java",
+  "Ruby",
+  "PHP",
+  "C++",
+  "CSS",
+  "C#",
+  "Go",
+  "C",
+  "TypeScript",
+  "Swift",
+];
+
+console.table(languages);
+
+languages.sort();
+languages.reverse();
+console.table(languages);
