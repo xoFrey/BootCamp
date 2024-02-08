92318963fe2679498bdc0f7ab62022b84669e7d6
xoFrey <xizelacar@gmail.com>
2024-02-08 14:02:02 +0100

    map1.3 done

---
+++ 2024-02-07-Arrays_part2/Übungen/map/assets/js/Array-Iteration-Level-1_3.js
@@ -0,0 +1,9 @@
+let array = [
+  18, 16, 80, 51, 47, 38, 95, 42, 68, 61, 34, 51, 20, 17, 56, 31, 100, 6, 5, 30,
+  74, 97, 28, 99, 91, 27, 73, 12, 92, 6, 27, 71, 26, 15, 78,
+];
+
+const doubleArray = array.map((number) => number * 2);
+console.log(doubleArray);
+doubleArray.sort((a, b) => a - b);
+console.table(doubleArray);
