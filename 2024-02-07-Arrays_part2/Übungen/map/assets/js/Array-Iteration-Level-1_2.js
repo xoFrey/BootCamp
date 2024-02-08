2d9081cd08807412e7988cfde7bf233b14326272
xoFrey <xizelacar@gmail.com>
2024-02-08 14:09:48 +0100

    Map 1-2

---
+++ 2024-02-07-Arrays_part2/Übungen/map/assets/js/Array-Iteration-Level-1_2.js
@@ -0,0 +1,13 @@
+const drinks = [
+  "Coca-Cola",
+  "Apfelsaft",
+  "Pepsi",
+  "Traubensaft",
+  "Sprite",
+  "Orangensaft",
+  "Red Bull Energy Drink",
+  "Fanta",
+];
+
+const upperDrinks = drinks.map((drink) => drink.toUpperCase());
+console.table(upperDrinks);
