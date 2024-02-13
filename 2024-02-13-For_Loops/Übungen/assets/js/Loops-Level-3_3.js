56e05ce672b106ce57bc6f6cca9a2122dcd8e1a0
xoFrey <xizelacar@gmail.com>
2024-02-13 13:17:53 +0100

    3.3 done

---
+++ 2024-02-13-For_Loops/Übungen/assets/js/Loops-Level-3_3.js
@@ -0,0 +1,25 @@
+const loopMe = () => {
+  const input = document.querySelector(".input").value;
+  const output = document.querySelector(".output");
+  let myArray = [];
+  if (input == 0) {
+    console.log("hofuvhndouvj");
+
+    output.innerHTML = `<p style="color:red"> Bitte Zahl über 0 eingeben! <p>`;
+  } else {
+    if (input % 2 === 0) {
+      for (let i = 1; i <= input; i++) {
+        myArray.push("o");
+      }
+    } else if (input % 2 != 0) {
+      for (let j = 0; j < input; j++) {
+        if (j % 2 == 0) {
+          myArray.push("o");
+        } else {
+          myArray.push("O");
+        }
+      }
+    }
+    output.innerHTML = `L${myArray.join("")}p`;
+  }
+};
