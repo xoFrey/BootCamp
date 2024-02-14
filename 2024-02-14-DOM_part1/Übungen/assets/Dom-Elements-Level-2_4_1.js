931659b7d1eb71097153897394de0f97340b4055
xoFrey <xizelacar@gmail.com>
2024-02-14 12:14:16 +0100

    changes

--- 2024-02-14-DOM_part1/Übungen/assets/Dom-Elements-Level-2_4_1.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector("body");
-const button = document.querySelector("#button");
-const colorPick = document.querySelector("#farbeAuswahlen");
-
-button.addEventListener("click", (event) => {
-  event.preventDefault();
-  body.style.backgroundColor = `${colorPick.value}`;
-});
