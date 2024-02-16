31689978a850d2b8cd6ad1ccaa94438bfead0b01
xoFrey <xizelacar@gmail.com>
2024-02-16 12:26:01 +0100

    1.5 done

---
+++ 2024-02-16-Objects/Übungen/assets/js/Objekte-Level-1_5.js
@@ -0,0 +1,16 @@
+let unserLager = {
+  schreibtisch: {
+    schublade: "Hefter",
+  },
+  schrank: {
+    "Obere Schublade": {
+      Ordner1: "Dokumente",
+      Ordner2: "Geheimnisse",
+    },
+    "Untere Schublade": "Cola",
+  },
+};
+
+console.log(unserLager.schrank["Obere Schublade"].Ordner2);
+console.log(unserLager.schreibtisch.schublade);
+console.log(unserLager.schrank["Untere Schublade"]);
