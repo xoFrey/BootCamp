0dcde32f41fa2475fd58b017c908bad38313dac3
xoFrey <xizelacar@gmail.com>
2024-02-20 13:28:05 +0100

    done

--- 2024-02-20-BOM_Timing_Functions/Übungen/BOM-Aufgaben/js/Bom-Level-1_3.js
+++ 2024-02-20-BOM_Timing_Functions/Übungen/BOM-Aufgaben/js/Bom-Level-1_3.js
@@ -1,9 +1,16 @@
 console.log(window.navigator);
+
+console.log(window.navigator.appCodeName);
+
 console.log(window.navigator.userAgent);
 console.log(window.navigator.platform);
+console.log(window.navigator.appVersion);
+
 console.log(window.screen.height);
 console.log(window.screen.width);
+
 console.log(window.innerHeight);
 console.log(window.innerWidth);
+
 console.log(window.screen.colorDepth);
 console.log(window.screen.pixelDepth);
