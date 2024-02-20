7c24937a08484990841583155b3f8afa5c095318
xoFrey <xizelacar@gmail.com>
2024-02-20 12:37:53 +0100

    ig 1.3 done???

---
+++ 2024-02-20-BOM_Timing_Functions/Übungen/js/Bom-Level-1_3.js
@@ -0,0 +1,9 @@
+console.log(window.navigator);
+console.log(window.navigator.userAgent);
+console.log(window.navigator.platform);
+console.log(window.screen.height);
+console.log(window.screen.width);
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+console.log(window.screen.colorDepth);
+console.log(window.screen.pixelDepth);
