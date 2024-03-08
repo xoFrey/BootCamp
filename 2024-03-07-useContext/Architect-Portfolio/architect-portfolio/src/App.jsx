56670c20a2d6543d5a141eb540696b56cccd8d35
xoFrey <xizelacar@gmail.com>
2024-03-08 08:09:30 +0100

    function done

--- 2024-03-07-useContext/Architect-Portfolio/architect-portfolio/src/App.jsx
+++ 2024-03-07-useContext/Architect-Portfolio/architect-portfolio/src/App.jsx
@@ -3,17 +3,36 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Gallery from "./pages/Gallery/Gallery";
 import Header from "./components/Header/Header";
+import { useState } from "react";
+import Loading from "./pages/Loading/Loading";
+import { DarkLightContext, LoadingContext } from "./context/Context";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [dark, setDark] = useState(false);
+
+  setTimeout(() => {
+    setLoading(true);
+  }, 2000);
   return (
-    <section className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-        </Routes>
-      </BrowserRouter>
-    </section>
+    <>
+      <DarkLightContext.Provider value={{ dark, setDark }}>
+        <LoadingContext.Provider value={{ loading, setLoading }}>
+          {loading ? (
+            <section className={`app ${dark && "dark"}`}>
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/gallery" element={<Gallery />} />
+                </Routes>
+              </BrowserRouter>
+            </section>
+          ) : (
+            <Loading />
+          )}
+        </LoadingContext.Provider>
+      </DarkLightContext.Provider>
+    </>
   );
 }
 
--- 2024-03-07-useContext/Architect-Portfolio/architect-portfolio/src/components/Header/Header.jsx
+++ 2024-03-07-useContext/Architect-Portfolio/architect-portfolio/src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import "./Header.css";
 import { Link, NavLink } from "react-router-dom";
 import Darkmode from "../../assets/svg/Darkmode/Darkmode";
+import Lightmode from "../../assets/svg/Lightmode/Lightmode";
 import Logo from "../../assets/svg/Logo/Logo";
+import { DarkLightContext } from "../../context/Context";
+import { useContext } from "react";
+
 const Header = () => {
+  const { dark, setDark } = useContext(DarkLightContext);
+
+  const darkLight = () => {
+    setDark((dark) => !dark);
+  };
   return (
     <header>
       <Link to="/">
@@ -14,9 +23,8 @@ const Header = () => {
         <NavLink to="/projects">Projects</NavLink>
         <NavLink to="/certification">Certification</NavLink>
         <NavLink to="/contacts">Contacts</NavLink>
-        <div>
-          <Darkmode />
-        </div>
+
+        <div onClick={darkLight}>{dark ? <Darkmode /> : <Lightmode />}</div>
       </nav>
     </header>
   );
